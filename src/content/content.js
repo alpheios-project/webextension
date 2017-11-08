dd052c731c3f486219e87a18e296a43f375b9196
Kirlat <kirlat@yula.media>
2017-11-08 17:07:19 +0400

    Code moved from inflection-tables.

---
+++ src/content/content.js
@@ -0,0 +1,8 @@
+import * as Content from './content-process'
+
+// load options, then render
+let contentProcess = new Content.Process()
+contentProcess.loadData().then(() => {
+  contentProcess.status = Content.Process.statuses.ACTIVE
+  contentProcess.render()
+})
