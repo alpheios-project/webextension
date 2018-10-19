edce70184cf1c99582f2df3030e1780ed3d6f701
Irina Sklyarova <irinasklyarova@Irinas-MacBook-Pro.local>
2018-10-19 20:51:44 +1000

    update links tp js and css

--- src/content-safari/index.js
+++ src/content-safari/index.js
@@ -4,6 +4,7 @@ import ContentProcess from '@safari/content/content-process'
 import ComponentStyles from 'alpheios-component-styles' // eslint-disable-line
 
 document.addEventListener('DOMContentLoaded', (event) => {
+  console.info('************DOMContentLoaded', event)
   let contentProcess = new ContentProcess()
   contentProcess.initialize()
 })
