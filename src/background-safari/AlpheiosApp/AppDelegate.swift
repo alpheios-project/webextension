c747c0ac33ca950a2a7e1c80d86c0c3bac68e75b
Bridget Almas <balmas@gmail.com>
2018-10-25 13:49:03 -0400

    a little cleanup for the build adding the alpheios icons to the app and extension assets changing the application ids cleaning up copyright statements

--- src/background-safari/AlpheiosApp/AppDelegate.swift
+++ src/background-safari/AlpheiosApp/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  AlpheiosV2
 //
 //  Created by Irina Sklyarova on 11/09/2018.
-//  Copyright © 2018 Irina Sklyarova. All rights reserved.
+//  Copyright © 2018 The Alpheios Project, Ltd.
 //
 
 import Cocoa
