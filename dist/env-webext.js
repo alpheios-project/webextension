8fe4f0b0f24f811890d3251ed00fe9b269aa10bc
Bridget Almas <balmsa@gmail.com>
2019-01-31 10:55:05 -0500

    remove file

--- dist/env-webext.js
+++
@@ -1,8 +0,0 @@
-const auth0Env = {
-  AUTH0_DOMAIN: 'alpheios.auth0.com',
-  AUTH0_CLIENT_ID: 'iT75HkBHThA4QdFwFoZRofLC41vVyvAt',
-  ENDPOINT: 'https://w2tfh159s2.execute-api.us-east-2.amazonaws.com/prod/words',
-  AUDIENCE: 'alpheios.net:apis',
-  LOGOUT_URL: 'https://alpheios.net/pages/logout',
-  TEST_ID: 'alpheiosMockUserIdlP0DWnmNxe'
-}
