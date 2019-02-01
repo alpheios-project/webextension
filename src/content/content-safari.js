0eaa00cfe81c9aa156fb78a50c77ddcbf784e5df
Kirlat <kirlat@yula.media>
2019-02-01 17:14:37 +0400

    Added an authentication module

--- src/content/content-safari.js
+++ src/content/content-safari.js
@@ -2,7 +2,9 @@
 import Message from '@/lib/messaging/message/message.js'
 import StateMessage from '@/lib/messaging/message/state-message'
 import MessagingService from '@/lib/messaging/service-safari.js'
-import { TabScript, UIController, LocalStorageArea, HTMLPage, L10n, enUS, Locales, enGB } from 'alpheios-components'
+import { TabScript, UIController, UIEventController, LocalStorageArea, HTMLPage, L10n, enUS, Locales, enGB,
+  LexicalQuery, ResourceQuery, AnnotationQuery, L10nModule, PanelModule, PopupModule,
+  MouseDblClick, LongTap, GenericEvt } from 'alpheios-components'
 import ComponentStyles from '../../node_modules/alpheios-components/dist/style/style-safari.min.css' // eslint-disable-line
 import Package from '../../package.json'
 
@@ -10,6 +12,68 @@ const pingInterval = 15000 // How often to ping background with a state message,
 let pingIntervalID = null
 let uiController = null
 let state = null
+
+/*
+A UI controller's builder function customized for Safari app extension
+ */
+const createUiController = (state, options) => {
+  let uiController = new UIController(state, options)
+
+  // Register data modules
+  uiController.registerDataModule(L10nModule, Locales.en_US, Locales.bundleArr())
+
+  // Register UI modules
+  uiController.registerUiModule(PanelModule, {
+    mountPoint: '#alpheios-panel', // To what element a panel will be mounted
+    panelComponent: 'panel', // A Vue component that will represent a panel
+    tabs: uiController.tabState // TODO: should be accessed via a public API, not via a direct link. This is a temporary solutions
+  })
+  uiController.registerUiModule(PopupModule, {
+    mountPoint: '#alpheios-popup',
+    uiController: uiController // Some child UI components require direct link to a uiController. TODO: remove during refactoring
+  })
+
+  // Creates on configures an event listener
+  let eventController = new UIEventController()
+  switch (uiController.options.textQueryTrigger) {
+    case 'dblClick':
+      eventController.registerListener('GetSelectedText', uiController.options.textQuerySelector, uiController.getSelectedText.bind(uiController), MouseDblClick)
+      break
+    case 'longTap':
+      eventController.registerListener('GetSelectedText', uiController.options.textQuerySelector, uiController.getSelectedText.bind(uiController), LongTap)
+      break
+    default:
+      eventController.registerListener(
+        'GetSelectedText', uiController.options.textQuerySelector, uiController.getSelectedText.bind(uiController), GenericEvt, uiController.options.textQueryTrigger
+      )
+  }
+
+  eventController.registerListener('HandleEscapeKey', document, uiController.handleEscapeKey.bind(uiController), GenericEvt, 'keydown')
+  eventController.registerListener('AlpheiosPageLoad', 'body', uiController.updateAnnotations.bind(uiController), GenericEvt, 'Alpheios_Page_Load')
+
+  // Attaches an event controller to a UIController instance
+  uiController.evc = eventController
+
+  // Subscribe to LexicalQuery events
+  LexicalQuery.evt.LEXICAL_QUERY_COMPLETE.sub(uiController.onLexicalQueryComplete.bind(uiController))
+  LexicalQuery.evt.MORPH_DATA_READY.sub(uiController.onMorphDataReady.bind(uiController))
+  LexicalQuery.evt.MORPH_DATA_NOTAVAILABLE.sub(uiController.onMorphDataNotFound.bind(uiController))
+  LexicalQuery.evt.HOMONYM_READY.sub(uiController.onHomonymReady.bind(uiController))
+  LexicalQuery.evt.LEMMA_TRANSL_READY.sub(uiController.updateTranslations.bind(uiController))
+  LexicalQuery.evt.WORD_USAGE_EXAMPLES_READY.sub(uiController.onWordUsageExamplesReady.bind(uiController))
+  LexicalQuery.evt.DEFS_READY.sub(uiController.onDefinitionsReady.bind(uiController))
+  LexicalQuery.evt.DEFS_NOT_FOUND.sub(uiController.onDefinitionsNotFound.bind(uiController))
+
+  // Subscribe to ResourceQuery events
+  ResourceQuery.evt.RESOURCE_QUERY_COMPLETE.sub(uiController.onResourceQueryComplete.bind(uiController))
+  ResourceQuery.evt.GRAMMAR_AVAILABLE.sub(uiController.onGrammarAvailable.bind(uiController))
+  ResourceQuery.evt.GRAMMAR_NOT_FOUND.sub(uiController.onGrammarNotFound.bind(uiController))
+
+  // Subscribe to AnnotationQuery events
+  AnnotationQuery.evt.ANNOTATIONS_AVAILABLE.sub(uiController.onAnnotationsAvailable.bind(uiController))
+
+  return uiController
+}
 
 /**
  * Activates a ping that will send state to background periodically
@@ -101,7 +165,7 @@ let handleStateRequest = async function handleStateRequest (message) {
    */
   if (!uiController) {
     if (diff.has('status') && diff.status === TabScript.statuses.script.ACTIVE) {
-      uiController = UIController.create(state, {
+      uiController = createUiController(state, {
         storageAdapter: LocalStorageArea,
         app: { name: 'Safari App Extension', version: `${Package.version}.${Package.build}` }
       })
