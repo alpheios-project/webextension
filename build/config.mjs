4e1aa3aba692beb4d25aece4322b9ec977420b14
Kirlat <kirlat@yula.media>
2019-07-11 16:38:04 +0400

    Updated a build process to use a minified version of components for production builds. Includes some other build improvements. Related to alpheios-project/node-build#11

---
+++ build/config.mjs
@@ -0,0 +1,37 @@
+import path from 'path'
+const projectRoot = process.cwd()
+
+const webpack = {
+  common: {
+    entry: {
+      background: './background/background.js',
+      content: './content/content.js'
+    }
+  },
+
+  production: {
+    mode: 'production',
+    output: { filename: '[name].js' },
+    resolve: {
+      alias: {
+        'alpheios-experience$': path.join(projectRoot, 'node_modules/alpheios-experience/dist/alpheios-experience.js'),
+        'alpheios-components$': path.join(projectRoot, 'node_modules/alpheios-components/dist/alpheios-components.min.js'),
+        '@': path.join(projectRoot, 'src')
+      }
+    }
+  },
+
+  development: {
+    mode: 'development',
+    output: { filename: '[name].js' },
+    resolve: {
+      alias: {
+        'alpheios-experience$': path.join(projectRoot, 'node_modules/alpheios-experience/dist/alpheios-experience.js'),
+        'alpheios-components$': path.join(projectRoot, 'node_modules/alpheios-components/dist/alpheios-components.js'),
+        '@': path.join(projectRoot, 'src')
+      }
+    }
+  }
+}
+
+export { webpack }
