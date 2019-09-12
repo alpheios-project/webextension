60fec59bb38d9ebf1ca23a17212ab9b33c75136c
kirlat <V2jQqsXmtpFQ7GTb3gPD>
2019-09-12 18:19:23 +0400

    Updated the containing app to check state of authentication in a persistent storage. UI updates and code cleanup.

--- src/safari-app-extension/AlpheiosReadingTools/AppDelegate.swift
+++ src/safari-app-extension/AlpheiosReadingTools/AppDelegate.swift
@@ -26,6 +26,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var loggedOutText: NSTextField!
     @IBOutlet weak var usernameTextInput: NSTextField!
     @IBOutlet weak var passwordTextInput: NSSecureTextField!
+    @IBOutlet weak var logInButton: NSButton!
     
     let headerText: String = "Alpheios Reading Tools"
     let headerIcon: NSImage! = NSImage.init(named: "AppIcon")
@@ -46,6 +47,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     // Whether the current user has been authenticated or not
     var isAuthenticated: Bool = false
     // Some user data that is required for the containing app
+    var userId: String?
     var userNickname: String?
     
     // An array to store a list of Auth0 users
@@ -91,34 +93,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return container
     }()
     
-    // MARK: - Core Data Saving and Undo support
-    
-    @IBAction func saveAction(_ sender: AnyObject?) {
-        #if DEBUG
-        os_log("Save action callback is executiong", log: OSLog.sAlpheios, type: .info)
-        #endif
+    func fetchAuthInfo() {
+        // Obtain a managed context
+        let managedContext = self.persistentContainer.viewContext
+        // Create a fetch request
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "AuthUser")
         
-        // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
-        let context = persistentContainer.viewContext
-        
-        if !context.commitEditing() {
-            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing before saving")
-        }
-        if context.hasChanges {
+        do {
+            // Check how many records are in the store
+            let recordsInStore = try managedContext.count(for: fetchRequest)
             #if DEBUG
-            os_log("Store context has changes", log: OSLog.sAlpheios, type: .info)
+            os_log("Persistent store has %d user records", log: OSLog.sAlpheios, type: .info, recordsInStore)
             #endif
-            do {
-                try context.save()
-                #if DEBUG
-                os_log("Store context has been changed", log: OSLog.sAlpheios, type: .info)
-                #endif
-            } catch {
-                // Customize this code block to include application-specific recovery steps.
-                let nserror = error as NSError
-                os_log("Cannot save tore context: %@", log: OSLog.sAlpheios, type: .error, nserror)
-                NSApplication.shared.presentError(nserror)
+            
+            // Returns an array of managed objects meeting the criteria specified by the fetch request
+            users = try managedContext.fetch(fetchRequest)
+            for user in users {
+                guard let userId = user.value(forKeyPath: "id") else {
+                    os_log("Obligatory AuthUser field, %s, is missing. Stored user authentication data will be ignored", log: OSLog.sAlpheios, type: .error, "id")
+                    return
+                }
+                guard let userNickname = user.value(forKeyPath: "nickname") else {
+                    os_log("Obligatory AuthUser field, %s, is missing. Stored user authentication data will be ignored", log: OSLog.sAlpheios, type: .error, "nickname")
+                    return
+                }
+                
+                self.isAuthenticated = true
+                self.userId = userId as? String ?? ""
+                self.userNickname = userNickname as? String ?? ""
             }
+            
+        } catch let error as NSError {
+            os_log("Could not fetch user authentication data. %@, %@", log: OSLog.sAlpheios, type: .error, error, error.userInfo)
         }
     }
     
@@ -126,6 +132,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         #if DEBUG
         os_log("applicationWillFinishLaunching CB", log: OSLog.sAlpheios, type: .info)
         #endif
+        self.fetchAuthInfo()
     }
     
     // Initial event of Application Launch
@@ -229,45 +236,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
-        // The code below is required to store all unsaved Core Data changes that are in memory but might not be saved to the hard drive yet. We do that before an application termination to make sure no in-memory changes will be lost.
-        let context = persistentContainer.viewContext
-        
-        if !context.commitEditing() {
-            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing to terminate")
-            return .terminateCancel
-        }
-        
-        if !context.hasChanges {
-            return .terminateNow
-        }
-        
-        do {
-            try context.save()
-        } catch {
-            let nserror = error as NSError
-            
-            // Customize this code block to include application-specific recovery steps.
-            let result = sender.presentError(nserror)
-            if (result) {
-                return .terminateCancel
-            }
-            
-            let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
-            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
-            let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
-            let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
-            let alert = NSAlert()
-            alert.messageText = question
-            alert.informativeText = info
-            alert.addButton(withTitle: quitButton)
-            alert.addButton(withTitle: cancelButton)
-            
-            let answer = alert.runModal()
-            if answer == .alertSecondButtonReturn {
-                return .terminateCancel
-            }
-        }
-        // If we got here, it is time to quit.
+        // Application is ready to terminate
         return .terminateNow
     }
     
@@ -283,7 +252,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let password = self.passwordTextInput.stringValue
         
         let authentication = Auth0.authentication()
-        self.loggedOutText.stringValue = "Please wait while we log you in..."
+        self.loggedOutText.stringValue = "Please wait while we are logging you in..."
         authentication.login(
             usernameOrEmail: username,
             password: password,
@@ -354,10 +323,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                                 #endif
                                 
                                 self.isAuthenticated = true
-                                self.updateAuthUI()
-                                // Clear text fields
-                                self.usernameTextInput.stringValue = ""
-                                self.passwordTextInput.stringValue = ""
                                 
                                 // Send user info to the extension
                                 let userInfo = [
@@ -367,6 +332,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                                     "accessToken": accessToken
                                 ]
                                 SFSafariApplication.dispatchMessage(withName: "UserLogin", toExtensionWithIdentifier: "net.alpheios.safari.ext", userInfo: userInfo, completionHandler: nil)
+                                
+                                // Clear text fields
+                                self.usernameTextInput.stringValue = ""
+                                self.passwordTextInput.stringValue = ""
+                                self.updateAuthUI()
                                 
                             case .failure(let error):
                                 os_log("User info retrieval failed: %@", log: OSLog.sAlpheios, type: .error, error as CVarArg)
@@ -397,13 +367,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         SFSafariApplication.dispatchMessage(withName: "UserLogout", toExtensionWithIdentifier: "net.alpheios.safari.ext", userInfo: nil, completionHandler: nil)
     }
     
-    @objc func authEventDidHappen(notification: NSNotification){
-        #if DEBUG
-        os_log("Auth event callback", log: OSLog.sAlpheios, type: .info)
-        #endif
-    }
-    
     func updateAuthUI() {
+        #if DEBUG
+        os_log("updateAuthUI() has been called, auth status is %s", log: OSLog.sAlpheios, type: .info, self.isAuthenticated.description)
+        #endif
         if (self.isAuthenticated) {
             if (self.userNickname != nil) {
                 self.loggedInText.stringValue = "You are logged in as \(self.userNickname!)"
@@ -412,65 +379,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
             self.loggedOutBox.isHidden = true
             self.loggedInBox.isHidden = false
-        } else {
-            // self.authText.stringValue = "You are logged out"
-            self.loggedOutText.stringValue = "Please use the form above to log in"
-            self.loggedOutBox.isHidden = false
-            self.loggedInBox.isHidden = true
-        }
-    }
-    
-    func saveAuthUser(email: String, authenticated: Bool) {
-        
-        // 1
-        let managedContext = self.persistentContainer.viewContext
-        
-        // 2
-        let entity =
-        NSEntityDescription.entity(forEntityName: "AuthUser", in: managedContext)!
-        
-        let user = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        // 3
-        user.setValue(email, forKeyPath: "email")
-        user.setValue(authenticated, forKeyPath: "authenticated")
-        
-        // 4
-        do {
-        try managedContext.save()
-            authUsers.append(user)
-            #if DEBUG
-            os_log("User data has been stored successfully", log: OSLog.sAlpheios, type: .info)
-            #endif
-        } catch let error as NSError {
-            os_log("Cannot save user data: %@, %@", log: OSLog.sAlpheios, type: .info, error, error.userInfo)
-        }
-    }
-    
-    func fetchUsers() {
-        /*Before you can do anything with Core Data, you need a managed object context. */
-        let managedContext = self.persistentContainer.viewContext
-        
-        /*As the name suggests, NSFetchRequest is the class responsible for fetching from Core Data.
-         
-         Initializing a fetch request with init(entityName:), fetches all objects of a particular entity. This is what you do here to fetch all Person entities.
-         */
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "AuthUser")
-        
-        /*You hand the fetch request over to the managed object context to do the heavy lifting. fetch(_:) returns an array of managed objects meeting the criteria specified by the fetch request.*/
-        do {
-            // Check how many records are in the store
-            let recordsInStore = try managedContext.count(for: fetchRequest)
-            #if DEBUG
-            os_log("Persistent store has %d user records", log: OSLog.sAlpheios, type: .info, recordsInStore)
-            #endif
+            // self.loggedOutBox.display()
+            // self.loggedInBox.display()
             
-            users = try managedContext.fetch(fetchRequest)
-            #if DEBUG
-            os_log("Retrieved %d user records from a persisten store", log: OSLog.sAlpheios, type: .info, users.count)
-            #endif
-        } catch let error as NSError {
-            os_log("Could not fetch. %@, %@", log: OSLog.sAlpheios, type: .error, error, error.userInfo)
+            // self.passwordTextInput.isEnabled = false
+            // Try to take focus away from input fields
+            self.window?.makeFirstResponder(self.logInButton)
+        } else {
+            self.loggedOutText.stringValue = "Please enter your credentials to log in"
+            self.loggedOutBox.isHidden = false
+            // self.usernameTextInput.isEnabled = true
+            // self.passwordTextInput.isEnabled = true
+            self.loggedInBox.isHidden = true
+            
+            // self.loggedOutBox.display()
+            // self.loggedInBox.display()
         }
     }
 }
