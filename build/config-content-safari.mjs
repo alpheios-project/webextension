f422071db18a19501d5a045dead99aaa9b51afa5
Kirlat <kirlat@yula.media>
2020-05-04 15:13:09 +0400

    Added a webpack plugin to write build info into Safari plist files, for #281

--- build/config-content-safari.mjs
+++ build/config-content-safari.mjs
@@ -1,9 +1,68 @@
 import VueLoaderPlugin from '../node_modules/vue-loader/lib/plugin.js'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import path from 'path'
+import fs from 'fs'
 const projectRoot = process.cwd()
 
+/**
+ * This plugin injects a version and a build name into Safari plist files.
+ */
+class plistPlugin {
+  /**
+   *
+   * @param {object} buildInfo - An object that contains the following build information:
+   * @param {string} buildInfo.branch - A name of a git branch that a code being built belongs to.
+   * @param {string} buildInfo.number - An string in the format of YYYYMMDDCCC, where
+   *         YYYY is a year, MM is a month, DD is a day, and CCC is a build timestamp.
+   * @param {string} buildInfo.name - A build name in the branch-name.YYYYMMDDCCC format.
+   */
+  constructor ({ buildInfo } = {}) {
+    this.buildInfo = buildInfo
+  }
+
+  apply (compiler) {
+    // The code below will run bu the `done` webpack hook
+    compiler.hooks.done.tapPromise('PlistPlugin', async (stats) => {
+      const projectRoot = process.cwd()
+      const pkgPath = path.join(projectRoot, 'package.json')
+      let pkg
+      try {
+        pkg = fs.readFileSync(pkgPath, 'utf8')
+      } catch (err) { console.error(`Cannot read ${pkgPath}: ${err.message}`); return }
+      pkg = JSON.parse(pkg)
+      let safariAppExtPlistPath = path.join(projectRoot, 'src/safari-app-extension/AlpheiosReadingTools/Info.plist')
+      let safariAppExtPlistContent
+      try {
+        safariAppExtPlistContent = fs.readFileSync(safariAppExtPlistPath, 'utf8')
+      } catch (err) { console.error(`Cannot read ${safariAppExtPlistPath}: ${err.message}`); return }
+      safariAppExtPlistContent = safariAppExtPlistContent.replace(/(<key>CFBundleShortVersionString<\/key>\s+<string>)(\S+)(<\/string>)/, `$1${pkg.version}$3`)
+      safariAppExtPlistContent = safariAppExtPlistContent.replace(/(<key>CFBundleVersion<\/key>\s+<string>)(\S+)(<\/string>)/, `$1${this.buildInfo.name}$3`)
+      try {
+        fs.writeFileSync(safariAppExtPlistPath, safariAppExtPlistContent)
+      } catch (err) { console.error(`Cannot write ${safariAppExtPlistPath}: ${err.message}`); return }
+
+      safariAppExtPlistPath = path.join(projectRoot, 'src/safari-app-extension/AlpheiosSafari/Info.plist')
+      try {
+        safariAppExtPlistContent = fs.readFileSync(safariAppExtPlistPath, 'utf8')
+      } catch (err) { console.error(`Cannot read ${safariAppExtPlistPath}: ${err.message}`); return }
+      safariAppExtPlistContent = safariAppExtPlistContent.replace(/(<key>CFBundleShortVersionString<\/key>\s+<string>)(\S+)(<\/string>)/, `$1${pkg.version}$3`)
+      safariAppExtPlistContent = safariAppExtPlistContent.replace(/(<key>CFBundleVersion<\/key>\s+<string>)(\S+)(<\/string>)/, `$1${this.buildInfo.name}$3`)
+      try {
+        fs.writeFileSync(safariAppExtPlistPath, safariAppExtPlistContent)
+      } catch (err) { console.error(`Cannot write ${safariAppExtPlistPath}: ${err.message}`) }
+    })
+  }
+}
+
 const webpack = {
+  custom: {
+    // The plist plugin must run in both production and development modes
+    common: {
+      plugins: [
+        plistPlugin
+      ]
+    }
+  },
   common: {
     context: path.join(projectRoot, 'src/content'),
     entry: './content-safari.js',
