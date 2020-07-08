002f08ead90cfd6bef362c1056df83dd82903581
Kirlat <kirlat@yula.media>
2020-07-08 20:52:32 +0400

    Updated an import path to avoid ambiguation

--- src/background/background.js
+++ src/background/background.js
@@ -6,7 +6,7 @@ const browserFeatures = new Browser().inspect().getFeatures()
 console.log(`Support of a "browser" namespace: ${browserFeatures.browserNamespace}`)
 if (!browserFeatures.browserNamespace) {
   console.log('"browser" namespace is not supported, will load a WebExtensions polyfill into the background script')
-  window.browser = require('../../dist/support/webextension-polyfill/browser-polyfill')
+  window.browser = require('../../dist/support/webextension-polyfill/browser-polyfill.min.js')
 }
 
 let backgroundProcess = new Process(browserFeatures) // eslint-disable-line prefer-const
