4da6e7216406473e28f288ba1c314c796f70ef1b
Kirlat <kirlat@yula.media>
2020-07-15 18:04:51 +0400

    Updated to build with an empty dist directory in accordance to Mozilla requirements

---
+++ src/env/env-webext-template.js
@@ -0,0 +1,10 @@
+const auth0Env = {
+  AUTH0_DOMAIN: 'alpheios.auth0.com',
+  AUTH0_CLIENT_ID: 'PLACE_AUTH0_CLIENT_ID_HERE',
+  ENDPOINTS: {
+    wordlist: 'https://userapis.alpheios.net/v1/words',
+    settings: 'https://settings.alpheios.net/v1/settings'
+  },
+  AUDIENCE: 'alpheios.net:apis',
+  LOGOUT_URL: 'https://alpheios.net/pages/logout'
+}
