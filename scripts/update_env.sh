061d1f0b76b808eb3e2d2839fdcb3bc5f397512b
Bridget Almas <balmsa@gmail.com>
2020-08-19 09:00:12 -0400

    fix location of safari env

--- scripts/update_env.sh
+++ scripts/update_env.sh
@@ -1,3 +1,3 @@
 sed -i "s/PLACE_AUTH0_CLIENT_ID_HERE/$1/g" dist/env-webext.js
-sed -i "s/PLACE_AUTH0_CLIENT_ID_HERE/$2/g" dist/env-safari-app-ext.js
+sed -i "s/PLACE_AUTH0_CLIENT_ID_HERE/$2/g" src/lib/auth/env-safari-app-ext.js
 
